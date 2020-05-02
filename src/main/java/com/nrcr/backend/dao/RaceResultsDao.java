1d3a60b69b549dbcb0634b3294ce55c758aeefd4
Tesneem <zohaib.tesneem@gmail.com>
2020-05-02 02:06:25 +0100

    files needed for Azure

--- src/main/java/com/nrcr/backend/dao/RaceResultsDao.java
+++
@@ -1,8 +0,0 @@
-package com.nrcr.backend.dao;
-
-import com.nrcr.backend.model.RaceResults;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RaceResultsDao extends MongoRepository<RaceResults, String> {
-
-}
